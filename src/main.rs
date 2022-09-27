430a3656776f277e628215c6f21af76f531a1527
H1rono <hronok66@gmail.com>
2022-09-27 15:44:42 +0900

    :recycle: Insert struct Config

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        Self {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let filename = &args[2];
-    println!("reading file {}...", filename);
-    let mut f = File::open(filename).expect("file not found");
+    let config = Config::new(&args);
+    println!("reading file {}...", &config.filename);
+    let mut f = File::open(&config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("error while reading file");
     println!("The file contents is:\n{}", contents);
