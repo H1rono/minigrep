c47962703e15dbd2db1a7c97fa794d5f72443075
H1rono <hronok66@gmail.com>
2022-09-27 16:12:29 +0900

    :recycle: Modify error handling
    make error while parsing arguments easy to understand

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 struct Config {
     query: String,
@@ -8,17 +9,23 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
-        Self {
+    fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Self {
             query: args[1].clone(),
             filename: args[2].clone(),
-        }
+        })
     }
 }
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("error occured while parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("reading file {}...", &config.filename);
     let mut f = File::open(&config.filename).expect("file not found");
     let mut contents = String::new();
