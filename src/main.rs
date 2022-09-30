046c5e79ef6f021f251b13e58b62a60e6829c28a
H1rono <hronok66@gmail.com>
2022-09-30 16:58:09 +0900

    ✨ Output error messages to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use std::process;
 fn main() {
     let args: Vec<_> = env::args().collect();
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("error occured while parsing arguments: {}", err);
+        eprintln!("error occured while parsing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
 }
